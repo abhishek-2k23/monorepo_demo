0326d40c7bc543849dd88694f69b2e3f0baefd70
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-01-14 16:41:28 +0530

    create packages/app

---
+++ packages/app/src/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+app.listen(5500, () => {
+    console.log("server is up at 5500")
+})
+
+app.get('/', (_,res) => {
+    res.send('welcome')
+})
