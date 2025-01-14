b6b0b802ef5affab6e3a19c1770aaadc2897fee2
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-01-14 16:42:06 +0530

    create vite+react package/ui and test configuration with vitest

---
+++ packages/ui/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    coverage: {
+      reporter: ["text", "html"],
+      include: ["src/**/*.{jsx,js,ts,vue}"],
+    },
+    setupFiles: './setupTest.js'
+  },
+})
