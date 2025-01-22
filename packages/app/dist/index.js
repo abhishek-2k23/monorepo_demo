56fe63e5831e5bbb3d6bbf5e60f063b8226e49fd
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-01-22 14:08:34 +0530

    prettier check

--- packages/app/dist/index.js
+++ packages/app/dist/index.js
@@ -1,13 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.listen(5500, () => {
-    console.log("server is up at 5500");
+  console.log("server is up at 5500");
 });
-app.get('/', (_, res) => {
-    res.send('welcome');
+app.get("/", (_, res) => {
+  res.send("welcome");
 });
--- packages/ui/__test__/App.test.jsx
+++ packages/ui/__test__/App.test.jsx
@@ -1,13 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import { it } from 'vitest';
-import { describe } from 'vitest';
-import App from '../src/App';
-import { expect } from 'vitest';
+import { render, screen } from "@testing-library/react";
+import { it } from "vitest";
+import { describe } from "vitest";
+import App from "../src/App";
+import { expect } from "vitest";
 
-describe('App Component', () => {
-  it('should have Welcome text', () => {
+describe("App Component", () => {
+  it("should have Welcome text", () => {
     render(<App />);
-    let message = screen.getByText('Welcome');
+    let message = screen.getByText("Welcome");
     expect(message).toBeVisible();
   });
 });
